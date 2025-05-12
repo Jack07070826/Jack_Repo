7a0272477707e9a00eece5959a9ab657d0611bc5
Jack07070826 <531524522@qq.com>
2025-05-12 20:18:58 +0800

    update

--- demo-project/client/vite.config.ts
+++ demo-project/client/vite.config.ts
@@ -5,6 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    sourcemap: process.env.NODE_ENV === "development",
+    sourcemap: process.env.NODE_ENV == "development",
   },
 });
